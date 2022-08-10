41d6be542eb3c7477607fff1af9d66a177770f5c
Mohammad <moe.rifaii77@gmail.com>
2022-08-10 19:19:49 +0300

    update backend url

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ cp index.html 404.html
 git init
 git add -A
 git commit -m 'Deploy'
-git push -f git@github.com:dev-rifaii/spotify-matcher-frontend.git master:gh-pages
+git push -f git@github.com:dev-rifaii/music-matcher-frontend.git master:gh-pages
 
 cd -
